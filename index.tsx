3af727ff2de90d2f544dad100835ad50a9399cae
alanloonlw-lab <alanloo.nlw@gmail.com>
2026-03-16 14:48:28 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // <--- THIS IS THE MISSING LINE
 import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
